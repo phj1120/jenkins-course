6366f6f1daf38d5c967252a3ab8e513e7534f8ee
PHJ <83655391+phj1120@users.noreply.github.com>
2022-05-02 16:42:57 +0900

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -13,11 +13,10 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install
-              mkdir -p /var/jenkins_home
-chown -R 1000:1000 /var/jenkins_home/
-docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts
-              ")
+        shell("npm install"
+              "mkdir -p /var/jenkins_home"
+              "chown -R 1000:1000 /var/jenkins_home/"
+              "docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d --name jenkins jenkins/jenkins:lts")
         shell("npm start")
     }
 }
